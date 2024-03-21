db45465e09d12612356f2229d342aee093a3dd42
Sam-Cowman <sam.p.cowman@gmail.com>
2024-03-21 12:18:56 -0500

    trying to utilize local storage

--- Assets/JS/form.js
+++ Assets/JS/form.js
@@ -2,17 +2,27 @@
 const lightToggle = document.getElementById('lightToggle');
 const container = document.querySelector('body');
 
+
+let username = localStorage.getItem('username');
+let title = localStorage.getItem('title');
+let content = localStorage.getItem('content');
+
 document.addEventListener('DOMContentLoaded', function() {
    
     document.getElementById('mindForm').addEventListener('submit', function(event) {
         event.preventDefault(); 
 
         // Retrieve the values from the form fields
+        var username = document.getElementById('username').value;
         var title = document.getElementById('title').value;
         var content = document.getElementById('content').value;
 
+        localStorage.setItem('blogUsername', username);
+        localStorage.setItem('blogTitle', title);
+        localStorage.setItem('blogContent', content);
+
         // Construct the URL of the new page with query parameters
-        var url = 'blog.html?title=' + encodeURIComponent(title) + '&content=' + encodeURIComponent(content);
+        var url = 'blog.html';
 
         // Redirect the user to the new page
         window.location.href = url;
