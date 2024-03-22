e99ec424eb5cd6408871a64ebacabca062518b23
Sam-Cowman <sam.p.cowman@gmail.com>
2024-03-22 09:40:44 -0500

    edited Javascript code to see functionality

--- Assets/JS/form.js
+++ Assets/JS/form.js
@@ -1,45 +1,44 @@
-
 const lightToggle = document.getElementById('lightToggle');
-const container = document.querySelector('body');
+    const container = document.querySelector('body');
 
-
-let username = localStorage.getItem('username');
-let title = localStorage.getItem('title');
-let content = localStorage.getItem('content');
 
 document.addEventListener('DOMContentLoaded', function() {
-   
+    // Function to handle form submission
     document.getElementById('mindForm').addEventListener('submit', function(event) {
-        event.preventDefault(); 
+        event.preventDefault();
 
         // Retrieve the values from the form fields
-        var username = document.getElementById('username').value;
-        var title = document.getElementById('title').value;
-        var content = document.getElementById('content').value;
+        const username = document.getElementById('username').value;
+        const title = document.getElementById('title').value;
+        const content = document.getElementById('content').value;
 
-        localStorage.setItem('blogUsername', username);
-        localStorage.setItem('blogTitle', title);
-        localStorage.setItem('blogContent', content);
+        // Check if any of the fields are empty
+        if (username === '' || title === '' || content === '') {
+            alert('Please complete all fields.');
+            return; // Exit the function to prevent form submission
+        }
 
-        // Construct the URL of the new page with query parameters
-        var url = 'blog.html';
+        // Store form data in localStorage
+        localStorage.setItem('username', username);
+        localStorage.setItem('title', title);
+        localStorage.setItem('content', content);
 
         // Redirect the user to the new page
-        window.location.href = url;
+        window.location.href = 'blog.html';
     });
-});
 
-let mode ='light';
+    let mode = 'light';
 
-lightToggle.addEventListener('click', function () {
-    // Toggle between light and dark mode 
-    if (mode === 'light') {
-        mode = 'dark';
-        container.classList.remove('light');
-        container.classList.add('dark');
-    } else {
-        mode = 'light';
-        container.classList.remove('dark');
-        container.classList.add('light');
-    }
+    lightToggle.addEventListener('click', function () {
+        // Toggle between light and dark mode 
+        if (mode === 'light') {
+            mode = 'dark';
+            container.classList.remove('light');
+            container.classList.add('dark');
+        } else {
+            mode = 'light';
+            container.classList.remove('dark');
+            container.classList.add('light');
+        }
+    });
 });
