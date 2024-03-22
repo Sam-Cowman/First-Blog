97ae0931a774173366955f382172c39c04533b3b
Sam-Cowman <sam.p.cowman@gmail.com>
2024-03-22 11:30:10 -0500

    added a for loop for entering in the info for li

--- Assets/JS/blog.js
+++ Assets/JS/blog.js
@@ -1,5 +1,6 @@
 const lightToggle = document.getElementById('lightToggle');
 const container = document.querySelector('body');
+const formHistory = JSON.parse(localStorage.getItem('formHistory')) || []
 
 document.getElementById("back").addEventListener("click", function() {
     window.location.href = "index.html"; // Navigate to the landing page (index.html)
@@ -20,6 +21,19 @@ lightToggle.addEventListener('click', function () {
     }
 });
 
-// Need to someone connect this to the landing page 
-const entry4 = document.getElementById('entry4')
-console.log(entry4)
+for(let i = 0; i<formHistory.length; i++){
+    const li=document.createElement('li')
+    const div=document.createElement('div')
+    const entry=document.createElement('p')
+    entry.textContent=`Entry ${i+1}`
+    const h2=document.createElement('h2')
+    h2.textContent=formHistory[i].title;
+    const hr =document.createElement('hr')
+    const content =document.createElement('p')
+    content.textContent=formHistory[i].content;
+    const user=document.createElement('p')
+    user.textContent=`Created by ${formHistory[i].username}`
+    div.append(h2, hr, content, user)
+    li.append(entry,div)
+    document.querySelector('.entries').append(li)
+}
